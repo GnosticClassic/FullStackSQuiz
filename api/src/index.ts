cc253613b5f9f4526b796e60bbc608bbcd9221e8
Zedamat <bahrezedamat@gmail.com>
2024-11-12 23:44:35 +0300

    Json Parsing MiddleWare

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,21 @@
-import express from 'express'
+//import { json } from 'express'
+import express, {json,urlencoded} from 'express'
 import quizRoutes from './routes/quizRoutes'
 
 
 const port = 3000
 const app = express()
 
-
+app.use(urlencoded({extended:false}))
+app.use(json());
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+//Initialize Routes
 app.use("/quizzes", quizRoutes);
+
+//server listening port
 app.listen(port, () => {
     console.log(`Server is running at htttp://localhost:${port}`)
 })
